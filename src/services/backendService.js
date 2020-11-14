f75df6b1a823c9719393d24f77930c5bbbed0292
Itamar.Podjarski <itamar.podjarski@fundbox.com>
2020-11-14 14:28:45 +0200

    Set backend to heroku

--- src/services/backendService.js
+++ src/services/backendService.js
@@ -1,7 +1,7 @@
 import * as axios from "axios";
 import { uploadImage } from "./firebaseService";
 
-axios.defaults.baseURL = "http://localhost:9200";
+axios.defaults.baseURL = "http://kumuna.herokuapp.com";
 axios.defaults.headers.common["Content-Type"] = "application/json";
 axios.defaults.headers.common["Accept"] = "application/json";
 
